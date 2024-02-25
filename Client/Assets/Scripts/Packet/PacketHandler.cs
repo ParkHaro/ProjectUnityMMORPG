f2d2fa08cf465bc019d9ab6547db7b8a9a342cb6
haro7488 <haro7488@gmail.com>
2024-02-25 17:36:53 +0900

    Define Client Game Protocol

--- Client/Assets/Scripts/Packet/PacketHandler.cs
+++ Client/Assets/Scripts/Packet/PacketHandler.cs
@@ -1,23 +1,48 @@
 ﻿using Google.Protobuf;
 using Google.Protobuf.Protocol;
 using ServerCore;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 class PacketHandler
 {
-	public static void S_ChatHandler(PacketSession session, IMessage packet)
-	{
-		S_Chat chatPacket = packet as S_Chat;
-		ServerSession serverSession = session as ServerSession;
-
-		Debug.Log(chatPacket.Context);
-	}
-
 	public static void S_EnterGameHandler(PacketSession session, IMessage packet)
 	{
 		S_EnterGame enterGamePacket = packet as S_EnterGame;
 		ServerSession serverSession = session as ServerSession;
+		
+		Debug.Log($"{nameof(S_EnterGameHandler)} / {enterGamePacket.Player}");
+	}
+
+	public static void S_LeaveGameHandler(PacketSession session, IMessage packet)
+	{
+		S_LeaveGame leaveGamePacket = packet as S_LeaveGame;
+		ServerSession serverSession = session as ServerSession;
+		
+		Debug.Log($"{nameof(S_LeaveGameHandler)}");
+	}
+
+	public static void S_SpawnHandler(PacketSession session, IMessage packet)
+	{
+		S_Spawn spawnPacket = packet as S_Spawn;
+		ServerSession serverSession = session as ServerSession;
+		
+		Debug.Log($"{nameof(S_SpawnHandler)} / {spawnPacket.Players}");
+
+	}
+
+	public static void S_DespawnHandler(PacketSession session, IMessage packet)
+	{
+		S_Despawn despawnPacket = packet as S_Despawn;
+		ServerSession serverSession = session as ServerSession;
+		
+		Debug.Log($"{nameof(S_DespawnHandler)}");
+	}
+
+	public static void S_MoveHandler(PacketSession session, IMessage packet)
+	{
+		S_Move movePacket = packet as S_Move;
+		ServerSession serverSession = session as ServerSession;
+		
+		Debug.Log($"{nameof(S_MoveHandler)}");
 	}
 }
