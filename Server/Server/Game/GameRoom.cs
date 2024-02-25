3d320ebe34022512a523e8679813a275cf18accd
haro7488 <haro7488@gmail.com>
2024-02-25 22:45:33 +0900

    Implement Server skill

--- Server/Server/Game/GameRoom.cs
+++ Server/Server/Game/GameRoom.cs
@@ -8,7 +8,7 @@ namespace Server.Game
     {
         private object _lock = new object();
         public int RoomId { get; set; }
-        
+
         private readonly List<Player> _players = new List<Player>();
 
         public void EnterGame(Player newPlayer)
@@ -28,7 +28,7 @@ namespace Server.Game
                     var enterPacket = new S_EnterGame();
                     enterPacket.Player = newPlayer.Info;
                     newPlayer.Session.Send(enterPacket);
-                    
+
                     S_Spawn spawnPacket = new S_Spawn();
                     foreach (var player in _players)
                     {
@@ -37,9 +37,10 @@ namespace Server.Game
                             spawnPacket.Players.Add(player.Info);
                         }
                     }
+
                     newPlayer.Session.Send(spawnPacket);
                 }
-                
+
                 // 다른 플레이어에게 정보 전송
                 {
                     var spawnPacket = new S_Spawn();
@@ -67,13 +68,13 @@ namespace Server.Game
 
                 _players.Remove(leavePlayer);
                 leavePlayer.Room = null;
-                
+
                 // 본인에게 정보 전송
                 {
                     S_LeaveGame leavePacket = new S_LeaveGame();
                     leavePlayer.Session.Send(leavePacket);
                 }
-                
+
                 // 다른 플레이어에게 정보 전송
                 {
                     S_Despawn despawnPacket = new S_Despawn();
@@ -86,6 +87,57 @@ namespace Server.Game
                         }
                     }
                 }
+            }
+        }
+
+        public void HandleMove(Player player, C_Move movePacket)
+        {
+            if (player == null)
+            {
+                return;
+            }
+
+            lock (_lock)
+            {
+                // TODO : 검증
+
+                var info = player.Info;
+                info.PosInfo = movePacket.PosInfo;
+
+                var resMovePacket = new S_Move();
+                resMovePacket.PlayerId = player.Info.PlayerId;
+                resMovePacket.PosInfo = movePacket.PosInfo;
+
+                Broadcast(resMovePacket);
+            }
+        }
+
+        public void HandleSkill(Player player, C_Skill skillPacket)
+        {
+            if (player == null)
+            {
+                return;
+            }
+
+            lock (_lock)
+            {
+                var info = player.Info;
+                if (info.PosInfo.State != CreatureState.Idle)
+                {
+                    return;
+                }
+                
+                // TODO : 스킬 사용 가능 여부 검증
+                
+                info.PosInfo.State = CreatureState.Skill;
+                S_Skill resSkillPacket = new S_Skill();
+                resSkillPacket.PlayerId = player.Info.PlayerId;
+                resSkillPacket.SkillInfo.SkillId = 1;
+                
+                Broadcast(resSkillPacket);
+                
+                // TODO : 데미지 판정
+                
             }
         }
 
